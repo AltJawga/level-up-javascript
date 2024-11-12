8d594073491369e176aa46edebe9f87c506dec0d
Chris <40653036+AltJawga@users.noreply.github.com>
2024-11-12 22:14:27 +0000

    01_04 complete

--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -31,3 +31,24 @@ class Book {
 }
 
 // Write your code here
+/* TechnicalBook class which extends Book class while adding
+   the edition variable.
+   */
+class TechnicalBook extends Book {
+  constructor(title, author, ISBN, numCopies, edition) {
+    super(title, author, ISBN, numCopies);
+    this.edition = edition;
+  };
+
+  /* Getter method for edition */
+  getEdition() {
+    return `The current version of this book is ${this.edition}`;
+  };
+}
+
+const WorldPolitics = new TechnicalBook("World Politics: Interests, Interactions, Institutions",
+  "Jeffry A. Frieden,  David A. Lake, Kenneth A. Schultz", "978-0393872231", 10, 5
+);
+
+console.log(WorldPolitics.availability);
+console.log(WorldPolitics.getEdition());
