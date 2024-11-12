30d9bae56ad8946bdb156fc33b3597033d1ffd81
Chris <40653036+AltJawga@users.noreply.github.com>
2024-11-12 22:21:52 +0000

    01_05 complete

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "AltJawga",
+  [password]: "123456",
+  age: 20
+};
+
+console.log(user.username);
+console.log(user.password);
