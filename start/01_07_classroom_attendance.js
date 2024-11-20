3c8f0a838582e439adf58c7665838cdcb6d34e38
Chris <40653036+AltJawga@users.noreply.github.com>
2024-11-20 21:29:52 +0000

    01_07 completed

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,19 @@
 // Write your code here
+function getStudents(classroom) {
+  const { hasTeachingAssistant, classList } = classroom;
+
+  let tacher, teachingAssistant, students;
+  if (!hasTeachingAssistant) {
+    [teacher, ...students] = classList;
+  }
+  else {
+    [teacher, teachingAssistant, ...students] = classList;
+  }
+
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}));
